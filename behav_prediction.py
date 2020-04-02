bf23d14fcd419c8e1913426bee28b04f0b183ee6
FedeClaudi <federicoclaudi@protonmail.com>
2020-04-02 17:54:11 +0100

    improvements to behav prediction

--- behav_prediction.py
+++ behav_prediction.py
@@ -12,9 +12,15 @@ from mpl_toolkits.mplot3d import Axes3D
 from sklearn.mixture import GaussianMixture as GMM
 import cv2
 import multiprocessing as mp
+from termcolor import cprint
+import plotly.graph_objects as go
+from sklearn import preprocessing
 
 from fcutils.maths.utils import interpolate_nans, get_random_rows_from_array
+from fcutils.maths.geometry import calc_ang_velocity
 from fcutils.plotting.colors import salmon
+from fcutils.plotting.colors import colorMap
+
 from fcutils.plotting.utils import clean_axes
 from fcutils.video.utils import get_cap_from_file, get_cap_selected_frame, get_video_params, open_cvwriter
 
@@ -22,6 +28,10 @@ from vgatPAG.database.db_tables import *
 from vgatPAG.paths import main_data_folder
 
 
+from vtkplotter import show, Spheres
+from vtkplotter import settings
+settings.useDepthPeeling = True 
+settings.useFXAA = True 
 
 
 class BehaviourPrediction:
@@ -42,7 +52,7 @@ class BehaviourPrediction:
         transform_queue_size=4.0,
         a=None,
         b=None,
-        random_state=None,
+        random_state=np.random.seed(0),
         angular_rp_forest=False,
         target_n_neighbors=-1,
         target_metric='categorical',
@@ -52,29 +62,50 @@ class BehaviourPrediction:
         verbose=False,
     )
 
-    def __init__(self, n_behav_classes=8, umapper=None):
+    def __init__(self, n_behav_classes=6, umapper=None):
         self.tracking = None
         self.n_behav_classes = n_behav_classes
         self.umapper = umapper
 
     # ------------------------------------ Fit ----------------------------------- #
-    def prep_recording_tracking_data(self, rec):
-        self.rec = rec
+    def prep_recording_tracking_data(self, recs):
+        self.rec = recs[0]
 
+        tracking = []
+        for rec in recs:
         # Fetch data
-        bp_tracking = pd.DataFrame((Trackings * Trackings.BodyPartTracking & f"rec_name='{rec}'" & "bp!='left_ear'" & "bp!='right_ear'").fetch())
-        bs_tracking = pd.DataFrame((Trackings * Trackings.BodySegmentTracking & f"rec_name='{rec}'").fetch())
+            bp_tracking = pd.DataFrame((Trackings * Trackings.BodyPartTracking & f"rec_name='{rec}'" & "bp!='left_ear'" & "bp!='right_ear'").fetch())
+            bs_tracking = pd.DataFrame((Trackings * Trackings.BodySegmentTracking & f"rec_name='{rec}'").fetch())
 
-        # Isolate relevant
-        speeds = np.vstack([s for s in bp_tracking.speed.values]).T
-        orientations = np.vstack([s for s in bs_tracking.orientation.values]).T
-        ang_vels = np.vstack([s for s in bs_tracking.angular_velocity.values]).T
-        lengths = np.vstack([s for s in bs_tracking.bone_length.values]).T
+            # Isolate relevant features
+            speeds = np.vstack([s for s in bp_tracking.speed.values]).T
+            ang_vels = np.vstack([s for s in bs_tracking.angular_velocity.values]).T
+            lengths = np.vstack([s for s in bs_tracking.bone_length.values]).T
 
-        # Concatenate
-        self.tracking = interpolate_nans(np.hstack([speeds, orientations, ang_vels, lengths]))
+            # Compute angles between body segments
+            head_body_angle = bs_tracking.loc[(bs_tracking.bp1 == 'snout')&(bs_tracking.bp2 == 'neck')].orientation.values[0] - \
+                                bs_tracking.loc[(bs_tracking.bp1 == 'neck')&(bs_tracking.bp2 == 'body')].orientation.values[0]
+            head_body_angle = head_body_angle.reshape(head_body_angle.shape[0], 1)
 
-        print(f"Collated tracking, found {self.tracking.shape[1]} features for {self.tracking.shape[0]} frames")        
+            body_tail_angle = bs_tracking.loc[(bs_tracking.bp1 == 'neck')&(bs_tracking.bp2 == 'body')].orientation.values[0] - \
+                                bs_tracking.loc[(bs_tracking.bp1 == 'body')&(bs_tracking.bp2 == 'tail_base')].orientation.values[0]
+            body_tail_angle = body_tail_angle.reshape(body_tail_angle.shape[0], 1)
+
+            # Compute angular velocities
+            head_body_ang_vel = calc_ang_velocity(head_body_angle.ravel())
+            head_body_ang_vel = head_body_ang_vel.reshape(head_body_ang_vel.shape[0], 1)
+
+            body_tail_ang_vel = calc_ang_velocity(body_tail_angle.ravel())
+            body_tail_ang_vel = body_tail_ang_vel.reshape(body_tail_ang_vel.shape[0], 1)
+
+            # Concatenate
+            features = [speeds, ang_vels, lengths, head_body_angle, body_tail_angle, head_body_ang_vel, body_tail_ang_vel]
+            tracking.append(interpolate_nans(np.hstack(features)))
+
+        self.tracking = preprocessing.scale(np.vstack(tracking))
+        # self.tracking = np.vstack(tracking)
+
+        cprint(f"\nCollated tracking, found {self.tracking.shape[1]} features for {self.tracking.shape[0]} frames", "green", attrs=['bold'])        
         return self.tracking
 
 
@@ -92,20 +123,20 @@ class BehaviourPrediction:
             data = get_random_rows_from_array(self.tracking, max_n_frames)
 
         # Apply umap
-        print("Applying umap")
+        cprint(f"\nFitting umap with {data.shape[0]} frames",  "green", attrs=['bold'])
         start = time.time()
         umapper = umap.UMAP(**umap_params)
         embedding = umapper.fit_transform(self.tracking[:max_n_frames, :])
         self.umapper = umapper
-        print("UMAP embedding took {} seconds".format(round(time.time() - start, 2)))
+        cprint("UMAP fitting took {} seconds".format(round(time.time() - start, 2)),  "green", attrs=['bold'])
 
         # Apply GMM to embedded data
-        print(f"Fitting GMM to extract {self.n_behav_classes} clusters")
+        cprint(f"Fitting GMM to extract {self.n_behav_classes} clusters",  "green", attrs=['bold'])
         labels = GMM(n_components=self.n_behav_classes).fit_predict(embedding)
 
         # Visualise results
         if plot:
-            self.visualise_embedding(embedding, labels)
+            self.visualise_embedding(embedding, labels, title="FIT outcome visuaisation")
 
         self.embedding = embedding
         return embedding
@@ -113,38 +144,46 @@ class BehaviourPrediction:
     # ------------------------------ Predicting data ----------------------------- #
     def predict_data(self, data, plot=False):
         start = time.time()
-        print(f"Predicting behaviour on {data.shape[0]} frames, using {self.n_behav_classes} behaviour classes")
+        cprint(f"\nPredicting behaviour on {data.shape[0]} frames, using {self.n_behav_classes} behaviour classes",  "green", attrs=['bold'])
         embedding = self.umapper.transform(data)
         labels = GMM(n_components=self.n_behav_classes).fit_predict(embedding)
-        print("Prediction took {} seconds".format(round(time.time() - start, 2)))
+        cprint("Prediction took {} seconds".format(round(time.time() - start, 2)),  "green", attrs=['bold'])
 
         # Visualise results
         if plot:
-            self.visualise_embedding(embedding, labels)
+            self.visualise_embedding(embedding, labels, title="Predicted frames")
 
         return embedding, labels
 
     # ------------------------------- Visualisation ------------------------------ #
-    def visualise_embedding(self, embedding, labels):
+    def visualise_embedding(self, embedding, labels, title=None):
         if embedding.shape[1] > 3: 
-            print("cannot plot wiht >3 components, skipping")
+            cprint("cannot plot wiht >3 components, skipping",  "red", attrs=['bold'])
             return
 
         elif embedding.shape[1] == 3:
-            f = plt.figure()
-            ax = f.add_subplot(111, projection='3d')
-            ax.scatter(embedding[:, 0], embedding[:, 1], embedding[:, 2], c=labels, cmap='tab20')
+            # Plot spehres with vtkplotter
+
+            cols = [colorMap(i, name="tab20", vmin=0, vmax=len(list(set(labels)))) for i in labels]
+            pts = [(embedding[i, 0], embedding[i, 1], embedding[i, 2]) for i, n in enumerate(cols)]
+           
+            # all have same radius but different colors:
+            s0 = Spheres(pts, c=cols, r=.1, res=12, alpha=.7)  
+            show(s0, axes=1, bg='white')
 
         else:
             f, ax = plt.subplots(figsize=(10, 10))
             ax.scatter(embedding[:, 0], embedding[:, 1], c=labels, cmap='tab20')
-            ax.set(title='UMAP embedding of tracking features')
-        clean_axes(f)
+
+            if title is not None: ax.set(title=title)
+            clean_axes(f)
+
+
 
     def make_behav_class_clip(self, args):
         behav_label, labels, savepath = args
 
-        print(f"Saving example clip for beahviour class {behav_label}")
+        cprint(f"Saving example clip for beahviour class {behav_label}",  "green", attrs=['bold'])
 
         # Get video file
         recdata = (Recording & f"rec_name='{self.rec}'").fetch(as_dict=True)[0]
@@ -162,7 +201,7 @@ class BehaviourPrediction:
         text_color             = (30, 220, 30)
 
         # Make frames
-        for i in range(50000):
+        for i in range(30000):
             ret, frame = cap.read()
             if not ret: break
 
@@ -176,7 +215,8 @@ class BehaviourPrediction:
                     lineType)
                 writer.write(frame)
         writer.release()
-        print(f"    finished saving example clip for beahviour class {behav_label}")
+        cprint(f"    finished saving example clip for beahviour class {behav_label}",  "green", attrs=['bold'])
+
 
     def make_all_behav_clips(self, labels):
         classes = list(set(labels))
@@ -189,7 +229,7 @@ class BehaviourPrediction:
 
 if __name__ == "__main__":
     # Select an example session for testing
-    rec = Recording().fetch("rec_name")[0]
+    rec = Recording().fetch("rec_name")[:3]
 
     bp = BehaviourPrediction()
 
@@ -197,13 +237,13 @@ if __name__ == "__main__":
 
     
     # Embedd features and plot
-    embedding  = bp.fit(max_n_frames=10000, plot=True,  n_components=3)
+    embedding  = bp.fit(max_n_frames=50000, plot=True,  n_components=3)
 
-    # Predict data    
-    embed, labels = bp.predict_data(tracking, plot=False)
+    # # Predict data    
+    # embed, labels = bp.predict_data(tracking, plot=False)
 
-    # Make videoclips
-    bp.make_all_behav_clips(labels)
+    # # Make videoclips
+    # bp.make_all_behav_clips(labels)
 
 
     plt.show()
