cba3ebf9ce960eac0b1b3fade94b3d4b857042a2
FedeClaudi <federicoclaudi@protonmail.com>
2020-04-07 13:45:06 +0100

    improvements and fixes

--- roi_overview.py
+++ roi_overview.py
@@ -33,9 +33,9 @@ np.warnings.filterwarnings('ignore')
 # ----------------------------------- Setup ---------------------------------- #
 # ? Define some params
 n_sec_pre = 2 
-n_sec_post = 6
+n_sec_post = 4
 
-DEBUG = False
+DEBUG = True
 
 mice = Mouse.fetch("mouse")
 
@@ -47,12 +47,12 @@ recordings = {m:{s:(Recording & f"sess_name='{s}'" & f"mouse='{m}'").fetch(as_di
 
 
 def create_figure():
-    ncols = 5
+    ncols = 6
     nrows = 5
         
-    f, axarr = plt.subplots(figsize=(25, 12), ncols=ncols, nrows=nrows)
+    f, axarr = plt.subplots(figsize=(25, 12), ncols=ncols, nrows=nrows, sharey='row')
 
-    titles = ["Aligned to stim onset", "Aligned to escape onset", "Aligned to at shelter", "Aligned to spont homing", "Aligned to spont runs"]
+    titles = ["Aligned to stim onset", "Aligned to escape onset", "Aligned to speed peak", "Aligned to at shelter", "Aligned to spont homing", "Aligned to spont runs"]
     ylables = ['Shelter dist, px', 'Speed, px/frame', 'Ang vel, def/frame', 'Raw ROI sig', 'Mean ROI sig',]
     all_axes = []
     for i, title in enumerate(titles):
@@ -65,10 +65,12 @@ def create_figure():
                 ax.set(ylabel=ylabel)
             if n == nrows-1:
                 ax.set(xlabel="Time (s)")
+            if "Ang vel" in ylabel:
+                ax.set(ylim=[-20, 20])
             axes.append(ax)
         all_axes.append(axes)
     
-    set_figure_subplots_aspect(left=0.05, right=0.95, bottom=0.05, top=0.93, wspace=0.1, hspace=0.3)
+    set_figure_subplots_aspect(left=0.06, right=0.97, bottom=0.05, top=0.93, wspace=0.17, hspace=0.3)
     clean_axes(f)
 
     return f, all_axes
@@ -172,6 +174,9 @@ def get_spont_homings(shelter_distance, speed, roi_sig, astims, vstims):
         # Check if thers roi sig
         if np.std(roi_sig[start-200:next_in_shelt+200]) < 0.1: continue
 
+        # Check that there's no errors
+        if shelter_distance[start] < 450: continue
+
         starts.append(start)
         good_outs.append(roi_out)
 
@@ -222,6 +227,9 @@ def get_roi_runs(shelter_distance, speed, roi_sig, astims, vstims):
         # Check if thers roi sig
         if np.std(roi_sig[start-200:next_in_roi+200]) < 0.1: continue
 
+        # Check that there's no errors
+        if shelter_distance[start] > 100: continue
+
         starts.append(start)
     return starts
 
@@ -245,9 +253,7 @@ for mouse in mice:
 
         
         figures, axes, paths, n_trials = [], [], [], []
-        stim_traces, escape_traces, shelt_traces = [], [], []
-        dist_traces = dict(stim_aligned=[], escape_aligned=[], shelter_aligned=[])
-        speed_traces = dict(stim_aligned=[], escape_aligned=[], shelter_aligned=[])
+        stim_traces, escape_traces, speed_traces, shelt_traces = [], [], [], []
         spont_traces, spont_runs_traces = [], []
         for n, roi_id in enumerate(roi_ids):
             f, axs = create_figure()
@@ -259,12 +265,10 @@ for mouse in mice:
 
             stim_traces.append([])
             escape_traces.append([])
+            speed_traces.append([])
             shelt_traces.append([])
             spont_traces.append([])
             spont_runs_traces.append([])
-
-            for k,v in dist_traces.items(): v.append([])
-            for k,v in speed_traces.items(): v.append([])
 
             if DEBUG: break
 
@@ -314,7 +318,7 @@ for mouse in mice:
 
             # LOOP OVER ROIS
             for r, (roi_id, rsig) in enumerate(zip(roi_ids, roi_sigs)):
-                f, (stim_aligned_axs, escape_aligned_axs, shelt_aligned_axs, spont_homing_axs, spont_runs_axs) = figures[r], axes[r]
+                f, (stim_aligned_axs, escape_aligned_axs, speed_aligned_axs, shelt_aligned_axs, spont_homing_axs, spont_runs_axs) = figures[r], axes[r]
                 f_path = paths[r]
             
                 for stims, colors in zip((astims, vstims), (astim_colors, vstim_colors)):
@@ -330,33 +334,35 @@ for mouse in mice:
                         except:
                             continue
 
+                        speed_peak = np.argmax(np.nan_to_num(speed[stim:stim+frames_post])) + stim
+
                         n_trials[r] += 1
 
                         # Plot aligned to stim onset
                         plot_traces(stim_aligned_axs, shelter_distance, speed, ang_vel, rsig, stim, color)
                         stim_traces[r].append(rsig[stim-frames_pre:stim+frames_post])
-                        dist_traces['stim_aligned'][r].append(shelter_distance[stim-frames_pre : stim+frames_post])
-                        speed_traces['stim_aligned'][r].append(speed[stim-frames_pre : stim+frames_post])
 
                         # Plot traces aligned to escape start
                         plot_traces(escape_aligned_axs, shelter_distance, speed, ang_vel, rsig, estart, color)
                         escape_traces[r].append(rsig[estart-frames_pre:estart+frames_post])
-                        dist_traces['escape_aligned'][r].append(shelter_distance[stim-frames_pre:stim+frames_post])
-                        speed_traces['escape_aligned'][r].append(speed[stim-frames_pre:stim+frames_post])
+
+                        # Plot aligned to speed peak
+                        plot_traces(speed_aligned_axs, shelter_distance, speed, ang_vel, rsig, speed_peak, color)
+                        speed_traces[r].append(rsig[speed_peak-frames_pre:speed_peak+frames_post])
 
                         # Plot alined to at shelter
                         plot_traces(shelt_aligned_axs, shelter_distance, speed, ang_vel, rsig, at_shelter, color)
                         shelt_traces[r].append(rsig[at_shelter-frames_pre:at_shelter+frames_post])
-                        dist_traces['shelter_aligned'][r].append(shelter_distance[stim-frames_pre:stim+frames_post])
-                        speed_traces['shelter_aligned'][r].append(speed[stim-frames_pre:stim+frames_post])
 
                 # Plot spont homings
                 for spont in spont_homings:
+                    if shelter_distance[spont + frames_post + frames_pre] > 100: continue
                     plot_traces(spont_homing_axs, shelter_distance, speed, ang_vel, rsig, spont, magenta)
                     spont_traces[r].append(rsig[spont-frames_pre:spont+frames_post])
 
                 # Plot spont runs
                 for spont in spont_runs:
+                    if shelter_distance[spont + frames_post + frames_pre] < 450: continue
                     plot_traces(spont_runs_axs, shelter_distance, speed, ang_vel, rsig, spont, orange)
                     spont_runs_traces[r].append(rsig[spont-frames_pre:spont+frames_post])
 
@@ -364,7 +370,7 @@ for mouse in mice:
 
         # Now plot averages
         for r, roi_id in enumerate(roi_ids):
-            f, (stim_aligned_axs, escape_aligned_axs, shelt_aligned_axs, spont_homing_axs, spont_runs_axs) = figures[r], axes[r]
+            f, (stim_aligned_axs, escape_aligned_axs, speed_aligned_axs, shelt_aligned_axs, spont_homing_axs, spont_runs_axs) = figures[r], axes[r]
 
             # Plt mean for spont homings
             if spont_traces[r]:
@@ -382,6 +388,8 @@ for mouse in mice:
                 plot_roi_mean_sig(stim_aligned_axs[4], stim_traces[r])
                 plot_roi_mean_sig(escape_aligned_axs[4], escape_traces[r])
                 plot_roi_mean_sig(shelt_aligned_axs[4], shelt_traces[r])
+                plot_roi_mean_sig(speed_aligned_axs[4], speed_traces[r])
+
 
             f.suptitle(f"{rec['mouse']} - {sess} - {roi_id} - {n_trials[r]} trials")
 
@@ -392,5 +400,12 @@ for mouse in mice:
             if DEBUG: break
         if DEBUG: break
 
-if DEBUG: plt.show()
-# TODO fix spont detection
+    if DEBUG: 
+        plt.show()
+        break
+
+
+# TODO get ROI rec times from Vanessas data
+# TODO look at overall correlation with running speed and shelter distance
+# TODO fix Y axes
+# TODO fix random ROI samples
